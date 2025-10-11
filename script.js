803aad37fe9033f72d07ac301ad7f74f302917bc
Prathamesh2603 <prathameshpawar645@gmail.com>
2025-10-11 21:46:16 +0530

    winning pattern added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let boxes = document.querySelector(".box"); //all the 9 buttons are accessed.
+let reset = document.querySelector("#reset-btn"); //reset button is accessed.
+
+// player gets alternate turns.
+let turn0 = true; //playerX, player0
+
+// Players winning patterns
+const winPatterns = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+]
 
